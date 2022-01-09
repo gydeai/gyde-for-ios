740594347309146b097e0d6d8808d92250d9b19d
Rushikesh Kulkarni <rushkulky91@gmail.com>
2022-01-09 21:06:45 +0530

    Setup

---
+++ Example/Pods/Target Support Files/gyde-ios/gyde-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_gyde_ios : NSObject
+@end
+@implementation PodsDummy_gyde_ios
+@end
---
+++ Example/gyde-ios/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  gyde-ios
+//
+//  Created by Rushikesh Kulkarni on 01/09/2022.
+//  Copyright (c) 2022 Rushikesh Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
