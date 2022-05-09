d2ba87e78ded0285c4bb80a87b67cebec3b31138
Rushikesh Kulkarni <rushkulky91@gmail.com>
2022-05-09 11:30:06 +0530

    Added framework (device)

---
+++ Gyde.framework/Headers/Gyde.h
@@ -0,0 +1,17 @@
+//
+//  Gyde.h
+//  Gyde
+//
+//  Created by Rushikesh Kulkarni on 30/04/22.
+//
+
+#import <Foundation/Foundation.h>
+//! Project version number for Gyde.
+FOUNDATION_EXPORT double GydeVersionNumber;
+
+//! Project version string for Gyde.
+FOUNDATION_EXPORT const unsigned char GydeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gyde/PublicHeader.h>
+
+
