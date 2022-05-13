4054c6f9c7054d7b9bbcbde48fd90444d4b31484
Rushikesh Kulkarni <rushkulky91@gmail.com>
2022-05-13 11:35:22 +0530

    Added XCFramework

---
+++ Frameworks/GydeFramework.xcframework/ios-arm64/GydeFramework.framework/Headers/GydeFramework.h
@@ -0,0 +1,18 @@
+//
+//  GydeFramework.h
+//  GydeFramework
+//
+//  Created by Chaitanya Pandit on 08/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GydeFramework.
+FOUNDATION_EXPORT double GydeFrameworkVersionNumber;
+
+//! Project version string for GydeFramework.
+FOUNDATION_EXPORT const unsigned char GydeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GydeFramework/PublicHeader.h>
+
+
